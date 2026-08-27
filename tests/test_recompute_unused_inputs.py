af015d9483eac6cf8e69ecba44eb8f4e475a7aa9
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-27 23:35:18 +0800

    Use public sum op in recompute regressions

--- tests/test_recompute_unused_inputs.py
+++ tests/test_recompute_unused_inputs.py
@@ -7,6 +7,7 @@ import numpy as np
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
 
+import engine.ops as ops
 from engine.recompute import recompute
 from engine.tensor import Tensor
 
@@ -15,7 +16,7 @@ def test_recompute_skips_unused_requires_grad_input():
     x = Tensor([2.0, -3.0], requires_grad=True)
     unused = Tensor([5.0, 7.0], requires_grad=True)
 
-    out = recompute(lambda used, ignored: (used * used).sum(), x, unused)
+    out = recompute(lambda used, ignored: ops.sum(used * used), x, unused)
     out.backward()
 
     np.testing.assert_array_equal(x.grad, np.array([4.0, -6.0]))
@@ -28,7 +29,7 @@ def test_recompute_preserves_existing_grad_on_unused_input():
     existing = np.array([3.0, -2.0])
     unused.grad = existing.copy()
 
-    out = recompute(lambda used, ignored: (used * 2.0).sum(), x, unused)
+    out = recompute(lambda used, ignored: ops.sum(used * 2.0), x, unused)
     out.backward()
 
     np.testing.assert_array_equal(x.grad, np.array([2.0, 2.0]))
@@ -44,7 +45,7 @@ def test_recompute_multi_output_skips_unused_input():
         x,
         unused,
     )
-    (first.sum() + second.sum()).backward()
+    (ops.sum(first) + ops.sum(second)).backward()
 
     np.testing.assert_array_equal(x.grad, np.array([5.0, 5.0]))
     assert unused.grad is None
@@ -55,7 +56,7 @@ def test_recompute_unused_input_does_not_block_parameter_gradient():
     unused = Tensor([6.0, 8.0], requires_grad=True)
     weight = Tensor([3.0, 5.0], requires_grad=True)
 
-    out = recompute(lambda used, ignored: (used * weight).sum(), x, unused)
+    out = recompute(lambda used, ignored: ops.sum(used * weight), x, unused)
     out.backward()
 
     np.testing.assert_array_equal(x.grad, weight.data)
@@ -66,7 +67,7 @@ def test_recompute_unused_input_does_not_block_parameter_gradient():
 def test_recompute_duplicate_input_only_accumulates_used_replay_copy():
     x = Tensor([2.0, -1.0], requires_grad=True)
 
-    out = recompute(lambda used, ignored: (used * 4.0).sum(), x, x)
+    out = recompute(lambda used, ignored: ops.sum(used * 4.0), x, x)
     out.backward()
 
     np.testing.assert_array_equal(x.grad, np.array([4.0, 4.0]))
