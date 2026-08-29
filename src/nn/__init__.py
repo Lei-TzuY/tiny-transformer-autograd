cbe332989e9603a424fd21957be3fb8d96a8706f
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-30 07:29:58 +0800

    Export grouped-query attention

--- src/nn/__init__.py
+++ src/nn/__init__.py
@@ -1,6 +1,7 @@
 from .module import Module
 from .layers import Linear, Embedding, LayerNorm, RMSNorm, Dropout
 from .attention import SelfAttention, MultiHeadAttention, RotaryEmbedding
+from .grouped_attention import GroupedQueryAttention
 from .transformer import FeedForward, SwiGLU, TransformerBlock, GPT
 from .beam import beam_generate
 from .streaming import stream_generate
