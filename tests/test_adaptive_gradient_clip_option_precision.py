ea357e88b632725239c3fab6aa2b257d2e28b1e8
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-31 04:39:07 +0800

    Test NumPy integer option subclass dispatch

--- tests/test_adaptive_gradient_clip_option_precision.py
+++ tests/test_adaptive_gradient_clip_option_precision.py
@@ -29,6 +29,14 @@ class IntSubclass(int):
         raise RuntimeError("unexpected Python int subclass dispatch")
 
 
+class NumPyIntSubclass(np.int64):
+    float_calls = 0
+
+    def __float__(self):
+        type(self).float_calls += 1
+        raise RuntimeError("unexpected NumPy integer subclass dispatch")
+
+
 def test_wider_finite_options_outside_float64_are_range_errors():
     if np.finfo(np.longdouble).max <= np.finfo(np.float64).max:
         pytest.skip("platform longdouble is not wider than float64")
@@ -84,3 +92,15 @@ def test_python_numeric_subclass_options_do_not_dispatch_float_hooks():
 
     assert FloatSubclass.float_calls == 0
     assert IntSubclass.float_calls == 0
+
+
+def test_numpy_integer_subclass_options_do_not_dispatch_float_hooks():
+    parameter = Tensor([1.0], requires_grad=True)
+    parameter.grad = np.array([0.0])
+    NumPyIntSubclass.float_calls = 0
+
+    with np.errstate(all="raise"):
+        assert adaptive_clip_grad_(parameter, clip_factor=NumPyIntSubclass(1)) == 0
+        assert adaptive_clip_grad_(parameter, eps=NumPyIntSubclass(2)) == 0
+
+    assert NumPyIntSubclass.float_calls == 0
