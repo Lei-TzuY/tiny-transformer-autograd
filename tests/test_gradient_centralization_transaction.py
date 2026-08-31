e389d993b9bb7e010d3c95d710b82c931c2027f4
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-01 01:41:56 +0800

    Test cross-parameter gradient value mutation rollback

--- tests/test_gradient_centralization_transaction.py
+++ tests/test_gradient_centralization_transaction.py
@@ -38,6 +38,20 @@ class _RebindOtherGradient(np.ndarray):
         self._target.grad = np.full_like(np.asarray(self._target.data), 99.0)
 
 
+class _MutateOtherGradient(np.ndarray):
+    def __new__(cls, values, target):
+        array = np.asarray(values, dtype=np.float64).view(cls)
+        array._target = target
+        return array
+
+    def __array_finalize__(self, obj):
+        self._target = getattr(obj, "_target", None)
+
+    def __setitem__(self, key, value):
+        np.ndarray.__setitem__(self, key, value)
+        np.ndarray.__setitem__(self._target.grad, Ellipsis, 99.0)
+
+
 def test_mutate_then_raise_destination_is_rolled_back():
     parameter = Tensor(np.zeros((1, 2), dtype=np.float64), requires_grad=True)
     gradient = _MutateThenRaise([[1.0, 3.0]])
@@ -63,6 +77,26 @@ def test_successful_write_cannot_rebind_another_parameter_gradient():
     victim_before = np.array(victim_gradient, copy=True)
 
     with pytest.raises(RuntimeError, match="gradient binding changed"):
+        centralize_gradients_([attacker, victim])
+
+    assert attacker.grad is attacker_gradient
+    assert victim.grad is victim_gradient
+    np.testing.assert_array_equal(attacker.grad, attacker_before)
+    np.testing.assert_array_equal(victim.grad, victim_before)
+
+
+def test_successful_write_cannot_mutate_another_noop_gradient_value():
+    victim = Tensor(np.zeros((1, 2), dtype=np.float64), requires_grad=True)
+    victim_gradient = np.array([[-1.0, 1.0]], dtype=np.float64)
+    victim.grad = victim_gradient
+
+    attacker = Tensor(np.zeros((1, 2), dtype=np.float64), requires_grad=True)
+    attacker_gradient = _MutateOtherGradient([[1.0, 3.0]], victim)
+    attacker.grad = attacker_gradient
+    attacker_before = np.array(attacker_gradient, copy=True)
+    victim_before = np.array(victim_gradient, copy=True)
+
+    with pytest.raises(RuntimeError, match="gradient value changed"):
         centralize_gradients_([attacker, victim])
 
     assert attacker.grad is attacker_gradient
