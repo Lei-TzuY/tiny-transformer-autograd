4f27b399884b45cf63bfa77085265a535fb6c1a9
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-02 02:40:50 +0800

    Test malformed parameter owner transaction metadata

--- tests/test_gradient_centralization_parameter_owner_transaction.py
+++ tests/test_gradient_centralization_parameter_owner_transaction.py
@@ -26,6 +26,20 @@ class _CorruptParameterOwnerOnWrite(np.ndarray):
         self._target.data._owner_ref = None
 
 
+class _ReplaceParameterOwnerWithObjectOnWrite(np.ndarray):
+    def __new__(cls, values, target):
+        array = np.asarray(values, dtype=np.float64).view(cls)
+        array._target = target
+        return array
+
+    def __array_finalize__(self, obj):
+        self._target = getattr(obj, "_target", None)
+
+    def __setitem__(self, key, value):
+        np.ndarray.__setitem__(self, key, value)
+        self._target.data._owner_ref = object()
+
+
 def test_gradient_write_cannot_detach_parameter_storage_owner():
     parameter = Tensor(np.zeros((1, 2), dtype=np.float64), requires_grad=True)
     gradient = _CorruptParameterOwnerOnWrite([[1.0, 3.0]], parameter)
@@ -44,6 +58,22 @@ def test_gradient_write_cannot_detach_parameter_storage_owner():
 
     parameter.data[...] = 1.0
     assert parameter._version == version_before + 1
+
+
+def test_gradient_write_cannot_replace_owner_with_non_weakref_metadata():
+    parameter = Tensor(np.zeros((1, 2), dtype=np.float64), requires_grad=True)
+    gradient = _ReplaceParameterOwnerWithObjectOnWrite([[1.0, 3.0]], parameter)
+    parameter.grad = gradient
+    gradient_before = np.array(gradient, copy=True)
+    owner_ref_before = parameter.data._owner_ref
+
+    with pytest.raises(RuntimeError, match="parameter data ownership changed"):
+        centralize_gradients_([parameter])
+
+    assert parameter.data._owner_ref is owner_ref_before
+    assert owner_ref_before() is parameter
+    assert parameter.grad is gradient
+    np.testing.assert_array_equal(parameter.grad, gradient_before)
 
 
 def test_parameter_storage_owner_must_be_a_real_weak_reference():
