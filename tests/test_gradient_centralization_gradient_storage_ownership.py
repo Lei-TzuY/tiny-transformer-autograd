c473f2fb4aca32b6590c7357264c62995f130d1e
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-01 23:43:07 +0800

    test: expose ndarray-subclass foreign gradient alias

--- tests/test_gradient_centralization_gradient_storage_ownership.py
+++ tests/test_gradient_centralization_gradient_storage_ownership.py
@@ -6,6 +6,10 @@ import pytest
 from engine import Tensor, centralize_gradients_
 
 
+class _GradientView(np.ndarray):
+    """Ordinary ndarray subclass used to preserve a foreign storage base chain."""
+
+
 def test_rejects_external_tensor_managed_gradient_storage_before_write():
     parameter = Tensor([[10.0, 20.0]], requires_grad=True)
     external = Tensor([[1.0, 3.0]], requires_grad=False)
@@ -46,6 +50,28 @@ def test_rejects_plain_ndarray_view_of_external_tensor_storage_before_write():
     assert external._version == external_version
 
 
+def test_rejects_ndarray_subclass_view_of_external_tensor_storage_before_write():
+    parameter = Tensor([[10.0, 20.0]], requires_grad=True)
+    external = Tensor([[1.0, 3.0]], requires_grad=False)
+    external_data = external.data
+    subclass_view = external_data.view(_GradientView)
+    parameter.grad = subclass_view
+
+    external_values = np.array(external_data, copy=True)
+    external_version = external._version
+
+    assert type(subclass_view) is _GradientView
+    assert np.shares_memory(subclass_view, external_data)
+
+    with pytest.raises(ValueError, match="Tensor-managed storage"):
+        centralize_gradients_([parameter])
+
+    assert parameter.grad is subclass_view
+    assert external.data is external_data
+    np.testing.assert_array_equal(external.data, external_values)
+    assert external._version == external_version
+
+
 def test_allows_external_tensor_managed_gradient_storage_when_centralization_is_noop():
     parameter = Tensor([[2.0, 2.0]], requires_grad=True)
     external = Tensor([[-1.0, 1.0]], requires_grad=False)
@@ -76,6 +102,24 @@ def test_allows_plain_ndarray_view_of_external_tensor_storage_when_noop():
     assert centralize_gradients_([parameter]) == 0
 
     assert parameter.grad is plain_view
+    assert external.data is external_data
+    np.testing.assert_array_equal(external.data, external_values)
+    assert external._version == external_version
+
+
+def test_allows_ndarray_subclass_view_of_external_tensor_storage_when_noop():
+    parameter = Tensor([[2.0, 2.0]], requires_grad=True)
+    external = Tensor([[-1.0, 1.0]], requires_grad=False)
+    external_data = external.data
+    subclass_view = external_data.view(_GradientView)
+    parameter.grad = subclass_view
+
+    external_values = np.array(external_data, copy=True)
+    external_version = external._version
+
+    assert centralize_gradients_([parameter]) == 0
+
+    assert parameter.grad is subclass_view
     assert external.data is external_data
     np.testing.assert_array_equal(external.data, external_values)
     assert external._version == external_version
