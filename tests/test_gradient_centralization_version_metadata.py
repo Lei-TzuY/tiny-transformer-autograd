d0040636f73ba3486162cf363302c0deb9c7c712
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-01 22:40:51 +0800

    test: pin mutation-version type transactionality

--- tests/test_gradient_centralization_version_metadata.py
+++ tests/test_gradient_centralization_version_metadata.py
@@ -10,6 +10,22 @@ def _parameter_with_gradient():
     return parameter
 
 
+class _VersionCorruptingGradient(np.ndarray):
+    def __new__(cls, values, parameter):
+        obj = np.asarray(values, dtype=np.float64).view(cls)
+        obj._parameter = parameter
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is not None:
+            self._parameter = getattr(obj, "_parameter", None)
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        if self._parameter is not None:
+            self._parameter._version = np.int64(self._parameter._version)
+
+
 @pytest.mark.parametrize("bad_version", [True, np.int64(0), 1.5, "0"])
 def test_centralization_rejects_noncanonical_mutation_version_before_write(bad_version):
     parameter = _parameter_with_gradient()
@@ -37,3 +53,19 @@ def test_centralization_rejects_negative_mutation_version_before_write():
     assert parameter.grad is gradient
     np.testing.assert_array_equal(gradient, before)
     assert parameter._version == -1
+
+
+def test_centralization_rejects_mutation_version_type_drift_during_write():
+    parameter = _parameter_with_gradient()
+    entry_version = parameter._version
+    gradient = _VersionCorruptingGradient(parameter.grad, parameter)
+    before = np.array(gradient, copy=True)
+    parameter.grad = gradient
+
+    with pytest.raises(RuntimeError, match="mutation version changed for parameter 0"):
+        centralize_gradients_(parameter)
+
+    assert parameter.grad is gradient
+    np.testing.assert_array_equal(gradient, before)
+    assert type(parameter._version) is int
+    assert parameter._version == entry_version
